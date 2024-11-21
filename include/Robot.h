6277d2ed5b3df41c9af070bef07c369c3a670146
Floriane <anatofloriane@gmail.com>
2024-11-21 13:03:45 +0100

    declaration de la classe Robot et definition de quelques methodes

--- include/Robot.h
+++ include/Robot.h
@@ -1,16 +1,31 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+#include <vector>
 
+
+enum class Direction { NORD, EST, SUD, OUEST };
 class Robot
 {
     public:
-        Robot();
-        virtual ~Robot();
+        Robot(Position position, Direction direction);
 
-    protected:
+        void avancer();
+        void tournerGauche();
+        void tournerDroite();
+        bool detecterObstacle() const;
+
+        void ajouterObservateur(Observateur* observateur);
+        void notifierObservateurs();
+
+        int getPosition() const;
+        char getDirectionRobot() const;
+
 
     private:
+        Position d_position;
+        Direction d_direction;
+        std::vector<Observateur*> d_observateurs;
 };
 
 #endif // ROBOT_H
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,11 +1,29 @@
 #include "Robot.h"
 
-Robot::Robot()
-{
-    //ctor
+
+Robot::Robot(Position position, Direction direction) : d_position(position), d_direction(direction) {}
+
+void Robot::avancer() {
+// Déplace le robot
+switch (d_direction) {
+case Direction::NORD: d_position.x--; break;
+case Direction::EST: d_position.y++; break;
+case Direction::SUD: d_position.x++; break;
+case Direction::OUEST: d_position.y--; break;
+}
+notifierObservateurs();
 }
 
-Robot::~Robot()
-{
-    //dtor
+void Robot::tournerGauche() {
+direction = static_cast<Direction>((static_cast<int>(d_direction) + 3) % 4);
+notifierObservateurs();
+}
+
+void Robot::tournerDroite() {
+direction = static_cast<Direction>((static_cast<int>(d_direction) + 1) % 4);
+notifierObservateurs();
+}
+
+void Robot::ajouterObservateur(Observateur* observateur) {
+d_observateurs.push_back(observateur);
 }
